91a60d365660ae16716d3a9915dbda3c0a70b6bb
Pascelle Saint-Laurent <pascelle.saintlaurent@gmail.com>
2016-05-10 16:05:34 -0400

    latest

--- js/app.js
+++ js/app.js
@@ -1,30 +1,32 @@
 $(document).ready(function(){
-	$('form').submit(function(e) { 
+	$('.sc-submit').click(function(e) { 
 		e.preventDefault();
-		var item = $('.textbox').val();
-		var removebutton = "<input type='submit' class='rmvbtn' value='remove'>"
-		var checkbox = "<input type='checkbox' class='chkbx'>"
-		$('.itemswanted').append("<li>" + checkbox + " " + item + " " + removebutton + "</li>");
+		var item = $('.sc-input').val();
+		var removebutton = "<input type='submit' class='sc-remove-item' value='remove'>";
+		var checkbox = "<input type='checkbox' class='sc-check'>";
+		$('.sc-list').append("<li>" + checkbox + " " + item + " " + removebutton + "</li>");
 	});
 
-	 /* $('.rmvbtn').click(function() { 
-		 console.log('item removed');
-		this.closest('li').remove();
-	 }); */
 
-
-	$('.itemswanted').on('click', '.rmvbtn', function() { console.log('item removed');
+	$('.sc-list').on('click', '.sc-remove-item', function() { console.log('item removed');
 		$(this).closest('li').remove();
 });
 
-	$('.itemswanted').on('click', '.chkbx', function() { console.log('item crossed out');
-		$(this).closest('li').css({
-			'text-decoration': 'line-through',
-			'text-decoration-color': '#000',
-		});
 
-	}); 
+$('.sc-list').on('click', '.sc-check', function () {
+	console.log($("input:checked"));
+	if ($("input:checked").length === 1) { // if checked
 
+		$(this).closest('li')
+			.css('text-decoration', 'line-through')
+			.css('text-decoration-color', 'black');
+
+	} else { // if not checked
+
+		$(this).closest('li')
+			.css('text-decoration', 'none')
+			.css('text-decoration-color', '#444');
+	};
 	
 });
 
